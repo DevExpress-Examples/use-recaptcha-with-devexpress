de38c942a72d3105ad2f3a6e8d71a55a1fc2e1da
DevExpressExampleBot <fakeEmail>
2021-12-17 10:34:56 +0300

    Source auto update [skip ci]

--- VB/ReCaptcha/Default.aspx.designer.vb
+++ VB/ReCaptcha/Default.aspx.designer.vb
@@ -20,6 +20,15 @@ Namespace ReCaptcha
         Protected form1 As Global.System.Web.UI.HtmlControls.HtmlForm
 
         ''' <summary>
+        ''' ASPxFormLayout1 control.
+        ''' </summary>
+        ''' <remarks>
+        ''' Auto-generated field.
+        ''' To modify move field declaration from designer file to code-behind file.
+        ''' </remarks>
+        Protected ASPxFormLayout1 As Global.DevExpress.Web.ASPxFormLayout
+
+        ''' <summary>
         ''' Name control.
         ''' </summary>
         ''' <remarks>
